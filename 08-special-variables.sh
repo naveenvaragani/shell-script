062064b247cacf959ffe670be7e2d867a648a6e7
naveenvaragani <naveenv2712@gmail.com>
2025-01-05 16:32:36 +0530

    conversation added

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,4 +7,5 @@ echo "present working directory: $pwd"
 echo "home directory of current user: $home"
 echo "which user is running the script: $user"
 echo "process id of current script: $$"
+sleep 60
 echo "process id of last command in background: $!"
