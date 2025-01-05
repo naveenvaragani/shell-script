494991768be86e0512da5f3ef6177884d0f33d2c
naveenvaragani <naveenv2712@gmail.com>
2025-01-05 17:04:57 +0530

    conversation added

--- 09-number-greatert100.sh
+++ 09-number-greatert100.sh
@@ -1,11 +1,14 @@
 #!bin/bash
 
-$NUMBER = $1
+NUMBER = $1
 
-if($NUMBER -gt 100)
+if[$NUMBER -gt 100]
 
-echo "Number is greater than 100"
+then
+   echo "Number is greater than 100"
 
 else 
 
-echo "Number is less than 100"
+   echo "Number is less than or equal to 100"
+
+fi   
